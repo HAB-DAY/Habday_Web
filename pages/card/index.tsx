d10c538f3f958bd7496b49386a7b5c645f8dd02a
Eunhyung Choi <milkiss0414@naver.com>
2023-07-14 00:37:09 +0900

    Fix: label htmlFor 웹접근성

--- pages/card/index.tsx
+++ pages/card/index.tsx
@@ -22,29 +22,42 @@ export default function Card() {
       onClickLeftButton={submitNewPay}
     >
       <Styled.Title>결제정보를 입력해주세요</Styled.Title>
-      <Styled.Form>
+      <Styled.Form name="new-pay">
         <Styled.InputContainer>
-          <Styled.Label>카드번호</Styled.Label>
+          <Styled.Label htmlFor="card-number">카드번호</Styled.Label>
           <Styled.Input
+            id="card-number"
             placeholder="0000 - 0000 - 0000 - 0000"
             onChange={(e) => setNewPayForm({ card_number: e.target.value })}
           />
         </Styled.InputContainer>
         <Styled.InputContainer>
-          <Styled.Label>카드 유효기간</Styled.Label>
-          <Styled.Input placeholder="YYYY - MM" onChange={(e) => setNewPayForm({ expiry: e.target.value })} />
+          <Styled.Label htmlFor="card-expiry">카드 유효기간</Styled.Label>
+          <Styled.Input
+            id="card-expiry"
+            placeholder="YYYY - MM"
+            onChange={(e) => setNewPayForm({ expiry: e.target.value })}
+          />
         </Styled.InputContainer>
         <Styled.InputContainer>
-          <Styled.Label>생년월일</Styled.Label>
-          <Styled.Input placeholder="6자리" onChange={(e) => setNewPayForm({ birth: e.target.value })} />
+          <Styled.Label htmlFor="birth">생년월일</Styled.Label>
+          <Styled.Input id="birth" placeholder="6자리" onChange={(e) => setNewPayForm({ birth: e.target.value })} />
         </Styled.InputContainer>
         <Styled.InputContainer>
-          <Styled.Label>비밀번호</Styled.Label>
-          <Styled.Input placeholder="앞 2자리" onChange={(e) => setNewPayForm({ pwd_2digit: e.target.value })} />
+          <Styled.Label htmlFor="card-pwd">비밀번호</Styled.Label>
+          <Styled.Input
+            id="card-pwd"
+            placeholder="앞 2자리"
+            onChange={(e) => setNewPayForm({ pwd_2digit: e.target.value })}
+          />
         </Styled.InputContainer>
         <Styled.InputContainer>
-          <Styled.Label>카드별칭</Styled.Label>
-          <Styled.Input placeholder="최대 6자리" onChange={(e) => setNewPayForm({ payment_name: e.target.value })} />
+          <Styled.Label htmlFor="card-name">카드별칭</Styled.Label>
+          <Styled.Input
+            id="card-name"
+            placeholder="최대 6자리"
+            onChange={(e) => setNewPayForm({ payment_name: e.target.value })}
+          />
         </Styled.InputContainer>
         <Styled.Message>* 본인 명의의 카드만 입력 가능합니다.</Styled.Message>
       </Styled.Form>
